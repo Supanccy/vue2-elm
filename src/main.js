14740c56c612b157e18149f99ae695a9908117f1
Supanccy <chenchaoyang2013@163.com>
2019-03-26 01:26:32 +0800

    添加注释，优化代码

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import routes from './router/router'
 import store from './store/'
 import {routerMode} from './config/env'
 import './config/rem'
+
+//引入FasterClick
 import FastClick from 'fastclick'
 
+
+/**
+ * 移动设备上的浏览器默认会在用户点击屏幕大约延迟300毫秒后才会触发点击事件，
+ * 这是为了检查用户是否在做双击。为了能够立即响应用户的点击事件，才有了FastClick。
+ */
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function() {
         FastClick.attach(document.body);
